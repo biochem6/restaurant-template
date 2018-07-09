9d6ea2bd9bb05ff78e97a4350eaa9fc34f97812a
biochem6 <aashland5@gmail.com>
2018-07-08 22:35:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+
+
+document.getElementById('menu').addEventListener('click', () => {
+    window.open('menu.html');
+}); 
+document.getElementById('contact').addEventListener('click', () => {
+    window.open('contact.html');
+});
+document.getElementById('gallery').addEventListener('click', () => {
+    window.open('gallery.html');
+});
+document.getElementById('about').addEventListener('click', () => {
+    window.open('about.html');
+});
+
+}
