a948bb2eeb9d1f66d3376f0c9da2108b731aa295
Andrew Ashland <aashland@atiaviation.com>
2018-08-08 13:25:58 -0400

    changed buttons to anchor tags.  put the header into its own css file.  still need to work on mobile view.  need to add functionality to facebook and twitter buttons

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 window.onload = () => {
 
 
-document.getElementById('menu').addEventListener('click', () => {
-    window.open('html/menu.html');
-}); 
-document.getElementById('contact').addEventListener('click', () => {
-    window.open('contact.html');
-});
-document.getElementById('gallery').addEventListener('click', () => {
-    window.open('html/gallery.html');
-});
-document.getElementById('about').addEventListener('click', () => {
-    window.open('about.html');
-});
 
 }
